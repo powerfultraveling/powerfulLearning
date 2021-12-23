3038080994104eff8a37e91b3972b45b830f7763
Your Name <you@example.com>
2021-12-23 13:02:05 +0800

    first post

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'powerfulLearing',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://powerfultraveling.github.io',
+  baseUrl: '/powerfulLearning/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'powerfultraveling', // Usually your GitHub org/user name.
+  projectName: 'powerfulLearning', // Usually your repo name.
 
   presets: [
     [
